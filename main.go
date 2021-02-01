9cee0c7e9c017681ac4d101af93aa529eaee51c7
Daniel Martí <mvdan@mvdan.cc>
2021-02-01 18:53:54 +0000

    add more menu items, look for ipfs

--- main.go
+++ main.go
@@ -2,28 +2,55 @@ package main
 
 import (
 	"encoding/base64"
+	"log"
+	"os/exec"
 
 	"github.com/getlantern/systray"
 )
 
+var ipfsBinary string
+
 func main() {
-	systray.Run(onReady, onExit)
+	var err error
+	ipfsBinary, err = exec.LookPath("ipfs")
+	if err != nil {
+		log.Fatalf("could not locate the ipfs binary: %v", err)
+	}
+
+	systray.Run(trayReady, trayExit)
 }
 
-func onReady() {
+func trayReady() {
 	icon, err := base64.StdEncoding.DecodeString(systrayIconOff)
 	if err != nil {
 		panic(err)
 	}
+
 	systray.SetIcon(icon)
 	systray.SetTooltip("IPFS Desktop")
-	mQuit := systray.AddMenuItem("Quit", "Stop the IPFS daemon and quit the app")
+
+	menuDaemon := systray.AddMenuItem("Start IPFS", "Start the IPFS daemon")
+
+	systray.AddSeparator()
+
+	menuVersion := systray.AddMenuItem("Version/bug TODO", "")
+	menuQuit := systray.AddMenuItem("Quit", "Stop the IPFS daemon and quit the app")
+
 	go func() {
-		<-mQuit.ClickedCh
-		systray.Quit()
+		for {
+			select {
+			case <-menuDaemon.ClickedCh:
+				println("TODO")
+
+			case <-menuQuit.ClickedCh:
+				systray.Quit()
+
+			case <-menuVersion.ClickedCh:
+				println("TODO")
+			}
+		}
 	}()
 }
 
-func onExit() {
-	// clean up here
+func trayExit() {
 }
