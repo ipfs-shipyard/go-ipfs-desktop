0ee0bcf2987ddfcf3e714fa78ecb7fbfe9d32cc8
Daniel Martí <mvdan@mvdan.cc>
2021-02-02 14:44:12 +0000

    implement starting and stopping of the daemon
    The menu item also gracefully transitions between the two states.
    
    Fixes #1.

--- assets.go
+++ assets.go
@@ -1,5 +1,10 @@
 package main
 
+import "encoding/base64"
+
+// TODO: the icons here are PNG, which work on linux/darwin, but probably not on
+// windows as it wants ICO.
+
 // systrayIconOff is base64-encoded from https://github.com/ipfs-shipyard/ipfs-desktop/blob/master/assets/icons/tray/off-22Template.png
 const systrayIconOff = `
 iVBORw0KGgoAAAANSUhEUgAAABIAAAAWCAYAAADNX8xBAAAAAXNSR0IArs4c6QAAAERlWElmTU0A
@@ -13,3 +18,11 @@ f/e5t+zD0cpYwQFM74JxFrQZQH1SVaP8zyxY6g3o5Vpqe8l7yi/b+zqBtokV9N5GsGkR58IIo598
 V1ZPUfEK7UMfafRh81vRuAt2uX8AH2UOfyG2hw+yhZJQGNkyNrPwIfpg54aH9R04t/iU4y8EJkxf
 Mb8umgAAAABJRU5ErkJggg==
 `
+
+func decodeAsset(src string) []byte {
+	bs, err := base64.StdEncoding.DecodeString(src)
+	if err != nil {
+		panic(err) // should not happen
+	}
+	return bs
+}
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/base64"
+	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 
 	"github.com/getlantern/systray"
 )
@@ -12,6 +14,8 @@ import (
 var ipfsBinary string
 
 func main() {
+	log.SetPrefix("[go-ipfs-desktop] ")
+
 	ipfsBinary, err := exec.LookPath("ipfs")
 	if err != nil {
 		log.Fatalf("could not locate the ipfs binary: %v", err)
@@ -23,23 +27,22 @@ func main() {
 }
 
 type trayDaemon struct {
+	// TODO: we might need some sort of lock, since clicks are async.
 	ipfsBinary string
+	ipfsCmd    *exec.Cmd
 
-	menuDaemon  *systray.MenuItem
-	menuVersion *systray.MenuItem
-	menuQuit    *systray.MenuItem
+	menuStartStop *systray.MenuItem
+	menuVersion   *systray.MenuItem
+	menuQuit      *systray.MenuItem
 }
 
 func (d *trayDaemon) onReady() {
-	icon, err := base64.StdEncoding.DecodeString(systrayIconOff)
-	if err != nil {
-		panic(err) // should not happen
-	}
-
-	systray.SetIcon(icon)
+	// TODO: transition the icon as we start/stop the daemon
+	systray.SetIcon(decodeAsset(systrayIconOff))
 	systray.SetTooltip("IPFS Desktop")
 
-	d.menuDaemon = systray.AddMenuItem("Start IPFS", "Start the IPFS daemon")
+	// This menu item is filled when we start the daemon below.
+	d.menuStartStop = systray.AddMenuItem("", "")
 
 	systray.AddSeparator()
 
@@ -53,12 +56,23 @@ func (d *trayDaemon) onReady() {
 			}
 		}
 	}()
+
+	// Start the daemon.
+	d.startIPFS()
+}
+
+func (d *trayDaemon) onExit() {
+	log.Println("exiting")
 }
 
 func (d *trayDaemon) handleClick() error {
 	select {
-	case <-d.menuDaemon.ClickedCh:
-		return fmt.Errorf("TODO")
+	case <-d.menuStartStop.ClickedCh:
+		if d.ipfsRunning() {
+			return d.stopIPFS()
+		} else {
+			return d.startIPFS()
+		}
 
 	case <-d.menuQuit.ClickedCh:
 		systray.Quit()
@@ -69,6 +83,66 @@ func (d *trayDaemon) handleClick() error {
 	}
 }
 
-func (d *trayDaemon) onExit() {
-	log.Println("exiting")
+func (d *trayDaemon) ipfsRunning() bool {
+	return d.ipfsCmd != nil
+}
+
+func (d *trayDaemon) startIPFS() error {
+	log.Println("starting the IPFS daemon")
+
+	d.menuStartStop.Disable() // while in progress
+
+	cmd := exec.Command(d.ipfsBinary, "daemon")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	// save stderr in case we crash
+	// TODO: use stderr
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "Daemon is ready") {
+			// The daemon has started, we're done.
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	log.Println("IPFS daemon ready")
+
+	d.ipfsCmd = cmd
+
+	d.menuStartStop.SetTitle("Stop IPFS")
+	d.menuStartStop.SetTooltip("Stop the IPFS daemon")
+	d.menuStartStop.Enable()
+	return nil
+}
+
+func (d *trayDaemon) stopIPFS() error {
+	log.Println("stopping the IPFS daemon")
+
+	d.menuStartStop.Disable() // while in progress
+
+	// TODO: Send os.Interrupt first, giving ipfs a few seconds to
+	// gracefully shut down. How to do that on Windows?
+	d.ipfsCmd.Process.Kill()
+	d.ipfsCmd.Wait()
+
+	d.ipfsCmd = nil
+
+	d.menuStartStop.SetTitle("Start IPFS")
+	d.menuStartStop.SetTooltip("Start the IPFS daemon")
+	d.menuStartStop.Enable()
+	return nil
 }
